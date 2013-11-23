bf3eeed46d30a773b7a590c06bd1fe2d349b4f51
Robert Dorsey <rtdorsey@vt.edu>
2013-11-23 14:07:55 -0500

    Untrack deployment files

--- ACIS5504.Azure/obj/Release/ACIS5504/event_manager/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from event_manager import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'event/(?P<event_id>\d+)/$', views.event, name='event'),
-)
